ecf4ba906b0ce0bbbce5fba88384457d083276d6
Marqueane <marqueane_64@hotmail.com>
2022-11-12 15:54:17 -0300

    #03 Checkpoint. Criação dos templates home e post_detail. Navegação entre páginas.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
@@ -7,4 +8,8 @@ class Post(models.Model):
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
     conteudo = models.TextField()
-    data_publicacao = models.DateTimeField(default=datetime.now())
+    data_publicacao = models.DateTimeField(default=timezone.now())
+
+    def __str__(self): #irá retornar o título do post no django admin. Quando for adicionado um Post.
+        return self.titulo
+

cb75c40b029145fa477020a3933ae495c72d6693
Marqueane <marqueane_64@hotmail.com>
2023-05-23 21:07:42 -0300

    Adciona template e formulário na aplicação 'contato'

--- contato/views.py
+++ contato/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.contrib import messages
+from contato.contato_form import FormContato
+
 
 def contato(request):
-    return render(request, 'contato/contato.html')
+    return render(request, 'contato/contato.html', {'form': FormContato()})
+
+def processa_contato(request):
+
+    if request.method == 'POST':
+        contato = FormContato(request.POST)
+        if contato.is_valid():
+            assunto = contato.cleaned_data['assunto']
+            messages.success(request, 'Mensagem encaminhada com sucesso!')
+            return render(request, 'contato/contato.html', {'form': contato})
+        else:
+            return render(request, 'contato/contato.html', {'form': contato})
+
+    return render(request, 'contato/contato.html', {'form': FormContato()})

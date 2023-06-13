f8abd011ac448163da5f5376fa4298413422a98d
Marqueane <marqueane_64@hotmail.com>
2023-06-13 15:13:50 -0300

    Criação de usuários, método de login e logout.

--- contas/urls.py
+++ contas/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('', views.criar_conta, name='criar_conta'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# htmx_urlpatterns = [
-#     path('criar_conta/htmx_valida_username', views.htmx_valida_username, name='htmx_valida_username'),
-#     path('criar_conta/htmx_valida_senha', views.htmx_valida_senha, name='htmx_valida_senha'),
-#     path('criar_conta/htmx_valida_email', views.htmx_valida_email, name='htmx_valida_email'),
-# ]
-#
-# urlpatterns += htmx_urlpatterns
+htmx_urlpatterns = [
+    path('criar_conta/htmx_valida_username', views.htmx_valida_username, name='htmx_valida_username'),
+    path('criar_conta/htmx_valida_senha', views.htmx_valida_senha, name='htmx_valida_senha'),
+    path('criar_conta/htmx_valida_email', views.htmx_valida_email, name='htmx_valida_email'),
+]
+
+urlpatterns += htmx_urlpatterns
--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles/'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

469a07f0e7a875c2c8d040436048e8e5f7c4216d
Marqueane <marqueane_64@hotmail.com>
2023-07-14 00:22:19 -0300

    Revert "Criação de usuários, método de login e logout."
    This reverts commit f8abd011ac448163da5f5376fa4298413422a98d.

--- core/settings.py
+++ core/settings.py
@@ -127,10 +127,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
